22a9ed43c99e8b7396596cae31e7a94af96e31dc
Suresh <ksuresh2k@gmail.com>
2023-02-12 23:48:12 +1100

    replaced chrome with edge in tst config

--- wdio.local.tst.headless.conf.ts
+++ wdio.local.tst.headless.conf.ts
@@ -4,22 +4,31 @@ export const config: WebdriverIO.Config = {
     ...sharedConfig,
     ...{
         services: ['selenium-standalone'],
-        capabilities: [{
-            browserName: 'chrome',
-            'goog:chromeOptions': {
-                args: [ '-headless', 'disable-infobars', 'disable-popup-blocking', 'disable-notifications','disable-extensions', 'start-maximized' ],
-                prefs: {
-                  'profile.managed_default_content_settings.popups' : 2,
-                  'profile.managed_default_content_settings.notifications' : 2,
-                }
-            }
-        },
-        /*{
+        capabilities: [
+        //     {
+        //     browserName: 'chrome',
+        //     'goog:chromeOptions': {
+        //         args: [ '-headless', 'disable-infobars', 'disable-popup-blocking', 'disable-notifications','disable-extensions', 'start-maximized' ],
+        //         prefs: {
+        //           'profile.managed_default_content_settings.popups' : 2,
+        //           'profile.managed_default_content_settings.notifications' : 2,
+        //         }
+        //     }
+        // },
+        {
             browserName: "firefox",
             "moz:firefoxOptions": {
                 args: ['-headless']
             },
-        }*/
+        },
+        {
+            browserName: 'MicrosoftEdge',
+            'ms:edgeOptions': {
+                args: ['--headless']
+            }
+
+        }
+
         ]
     }
 }
