be363bc285b0680f99884f4377a84bcbb39087da
Suresh <ksuresh2k@gmail.com>
2023-02-13 00:22:22 +1100

    config updated for chrome options

--- wdio.local.tst.headless.conf.ts
+++ wdio.local.tst.headless.conf.ts
@@ -8,11 +8,12 @@ export const config: WebdriverIO.Config = {
             {
             browserName: 'chrome',
             'goog:chromeOptions': {
-                args: [ '-headless', 'disable-infobars', 'disable-popup-blocking', 'disable-notifications','disable-extensions', 'start-maximized' ],
-                prefs: {
-                  'profile.managed_default_content_settings.popups' : 2,
-                  'profile.managed_default_content_settings.notifications' : 2,
-                }
+                args: ['-headless']
+                // args: [ '-headless', 'disable-infobars', 'disable-popup-blocking', 'disable-notifications','disable-extensions', 'start-maximized' ],
+                // prefs: {
+                //   'profile.managed_default_content_settings.popups' : 2,
+                //   'profile.managed_default_content_settings.notifications' : 2,
+                // }
             }
         },
         // {
