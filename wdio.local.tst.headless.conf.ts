808938255852c9aafe2303927627637900224959
Suresh <ksuresh2k@gmail.com>
2023-02-13 00:09:47 +1100

    yaml file updated to install latest chrome

--- wdio.local.tst.headless.conf.ts
+++ wdio.local.tst.headless.conf.ts
@@ -5,29 +5,29 @@ export const config: WebdriverIO.Config = {
     ...{
         services: ['selenium-standalone'],
         capabilities: [
-        //     {
-        //     browserName: 'chrome',
-        //     'goog:chromeOptions': {
-        //         args: [ '-headless', 'disable-infobars', 'disable-popup-blocking', 'disable-notifications','disable-extensions', 'start-maximized' ],
-        //         prefs: {
-        //           'profile.managed_default_content_settings.popups' : 2,
-        //           'profile.managed_default_content_settings.notifications' : 2,
-        //         }
-        //     }
-        // },
-        {
-            browserName: "firefox",
-            "moz:firefoxOptions": {
-                args: ['-headless']
-            },
-        },
-        {
-            browserName: 'MicrosoftEdge',
-            'ms:edgeOptions': {
-                args: ['--headless']
+            {
+            browserName: 'chrome',
+            'goog:chromeOptions': {
+                args: [ '-headless', 'disable-infobars', 'disable-popup-blocking', 'disable-notifications','disable-extensions', 'start-maximized' ],
+                prefs: {
+                  'profile.managed_default_content_settings.popups' : 2,
+                  'profile.managed_default_content_settings.notifications' : 2,
+                }
             }
+        },
+        // {
+        //     browserName: "firefox",
+        //     "moz:firefoxOptions": {
+        //         args: ['-headless']
+        //     },
+        // },
+        // {
+        //     browserName: 'MicrosoftEdge',
+        //     'ms:edgeOptions': {
+        //         args: ['--headless']
+        //     }
 
-        }
+        // }
 
         ]
     }
